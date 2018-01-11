c4dbec40e8363ec4a1854a8c8eb7ff0caaaa7302
Madis-Karli Koppel <matukoppel@gmail.com>
2018-01-11 17:58:15 +0200

    edit hbase locataion
    update destination
    update created table name

--- python/hbase.py
+++ python/hbase.py
@@ -1,22 +1,35 @@
 import happybase
 
-# connection = happybase.Connection('192.168.99.100')
-connection = happybase.Connection('localhost')
-table = connection.table(b'Test5', use_prefix=False)
+table_name = b'koppel_test'
+
+connection = happybase.Connection('ir-hadoop1')
 
 # Show all available tables
 print(connection.tables())
+
+# Create a new table
+#if not connection.is_table_enabled(table_name):
+connection.create_table(
+	table_name,
+	{'cf1': dict(max_versions=10),
+         'cf2': dict(max_versions=1, block_cache_enabled=False),
+         'cf3': dict(),  # use defaults
+         }
+)
+
+table = connection.table(table_name, use_prefix=False)
+
 
 # Read all data in table
 for key, data in table.scan():
     print(key, data)
 
 # Insert values into table
-# table.put('1', {b'cf1:asi': b'value1'})
-# table.put('2', {b'cf1:asi': b'value2'})
-# table.put('3', {b'cf1:asi': b'value3'})
-# table.put('4', {b'cf1:asi': b'value4'})
-# table.put('5', {b'cf1:asi': b'value5'})
+table.put('1', {b'cf1:asi': b'value1'})
+table.put('2', {b'cf1:asi': b'value2'})
+table.put('3', {b'cf1:asi': b'value3'})
+table.put('4', {b'cf1:asi': b'value4'})
+table.put('5', {b'cf1:asi': b'value5'})
 
 # More specific printing
 # row = table.row(b'row-key')
@@ -28,14 +41,3 @@ for key, data in table.scan(row_prefix=b'1'):
 
 # Delete from table by rowID
 # row = table.delete(b'1')
-
-
-# Create a new table
-if not connection.is_table_enabled('mytable'):
-    connection.create_table(
-        'mytable',
-        {'cf1': dict(max_versions=10),
-         'cf2': dict(max_versions=1, block_cache_enabled=False),
-         'cf3': dict(),  # use defaults
-         }
-    )
