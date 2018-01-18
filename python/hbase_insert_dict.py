1ee215fa2da72dc148c763d7423c9416810c95c4
Madis-Karli Koppel <matukoppel@gmail.com>
2018-01-18 14:34:11 +0200

    split into files based on table structure

--- python/hbase_insert_dict.py
+++ python/hbase_insert_dict.py
@@ -1,17 +1,17 @@
 from pyspark import SparkContext, SparkConf
 
 import HBaseController
-import hashlib
+from ast import literal_eval as make_tuple
 
 import argparse
 
-# for reading sequence files: key, value pairs that have data like:
-# \00=<http::www.bestpractice.ee::/robots.txt::null::20150215040001\00\00\00\009\00\0
+# for reading the plaintext files that contain dictionary data like:
+# {'uncrossMatchedEntities_ORG': [(u'main', 2)], 'singleMatchedEntities_PER': [], 'crossMatchedEntities_PER': [] ...
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", help="path to input", required=True)
-parser.add_argument("-t", "--table_name", help="table name in hbase", default='koppel_test')
+parser.add_argument("-t", "--table_name", help="table name in hbase", default='koppel_test2')
 parser.add_argument("-hb", "--hbase_location", help="HBase ip", default='ir-hadoop1')
 parser.add_argument("-bs", "--batch_size",
                     help="batch size for hbase table. Shows how many values it inserts at one time",
@@ -41,12 +41,20 @@ def send_partition(parts):
     hc.stop()
 
 
-def map_key_value(row):
-    id_parts = row[0].split("::")
-    try:
-        return row[0], {'domain:main': id_parts[1], 'domain:sub': id_parts[2], 'text:all': row[1]}
-    except IndexError:
-        return row[0], {'text:all': row[1]}
+def map_dict(row):
+    t = make_tuple(row)
+    values = eval(str(t[1]))
+
+    out = {}
+    for k, v in values.iteritems():
+        if k == "hostname":
+            out['domain:main'] = str(v)
+        elif k == "path":
+            out['domain:sub'] = str(v)
+        else:
+            out['text:' + k] = str(v)
+
+    return str(t[0]), out
 
 
 # Start Spark
@@ -57,13 +65,12 @@ print "Spark Started"
 print "Reading data from: " + filename
 
 # Read the files
-reader = sc.sequenceFile(filename, "org.apache.hadoop.io.Text", "org.apache.hadoop.io.Text")
+reader = sc.textFile(filename)
 
-# Map the files into RDD that has key, value for inserting into HBase
-data2 = reader.map(map_key_value)
+# Map every line into an RDD containing dictionary ({})
+data = reader.map(map_dict)
 
-# Insert RDD into HBase, partition by partition
-data2.foreachPartition(send_partition)
+data.foreachPartition(send_partition)
 
 print('done')
 sc.stop()
